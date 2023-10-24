fed7dcbabacb2daecd808ab32eaccc30f4d61296
Hugo Chantelauze <hugo@etamin.studio>
2023-10-24 12:55:59 +0200

    fix json todayScore and add info in the userHome

--- src/pages/UserHome/UserHome.tsx
+++ src/pages/UserHome/UserHome.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useParams } from "react-router-dom";
 import userMainData from "../../service/mocked_data/userMainData";
 import Heading from "../../components/Heading/Heading";
@@ -7,10 +8,44 @@ export default function UserHome() {
 
   const user = userMainData.find((userData) => userData.id.toString() === userId);
 
+  const scorePercentage = user && user.todayScore ? (user.todayScore * 100).toFixed(0) : null;
+
   return (
     <div>
       {user ? (
-        <Heading name={`${user.userInfos.firstName}`} />
+        <div>
+          <Heading name={`${user.userInfos.firstName}`} />
+          <p>
+            <strong>Prénom :</strong> {user.userInfos.firstName}
+          </p>
+          <p>
+            <strong>Nom :</strong> {user.userInfos.lastName}
+          </p>
+          <p>
+            <strong>Âge :</strong> {user.userInfos.age}
+          </p>
+          <p>
+            <strong>Score aujourd'hui :</strong>{" "}
+            {scorePercentage !== null ? `${scorePercentage}%` : "N/A"}
+          </p>
+          <p>
+            <strong>Données clés :</strong>
+            <ul>
+              <li>
+                <strong>Calories :</strong> {user.keyData.calorieCount}
+              </li>
+              <li>
+                <strong>Protéines :</strong> {user.keyData.proteinCount}
+              </li>
+              <li>
+                <strong>Glucides :</strong> {user.keyData.carbohydrateCount}
+              </li>
+              <li>
+                <strong>Lipides :</strong> {user.keyData.lipidCount}
+              </li>
+            </ul>
+          </p>
+        </div>
       ) : (
         <p>Utilisateur non trouvé</p>
       )}
