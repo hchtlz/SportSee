cf9f7bdc89159ea7bebcdae6b29fae37835fd570
Hugo Chantelauze <hugo@etamin.studio>
2023-10-24 21:00:29 +0200

    separated css to barchart

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -2,58 +2,7 @@ import { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, ResponsiveContainer, TooltipProps} from 'recharts';
 import { useParams } from 'react-router-dom';
 import { getUserActivity } from '../../service/api/data';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-    margin-bottom: 2em;
-	height: 320px;
-    width: 835px;
-	border-radius: 5px;
-    background: #FBFBFB;
-	box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.0212249);
-	padding: 25px;
-	
-	@media (max-width: 1025px) {
-      padding-left:0;
-    }
-`
-const Head = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 2.5rem;
-    margin-right: 1.5rem;
-	`
-
-const Title= styled.h2`
-    font-size: 15px;
-    line-height: 24px;
-    color: #20253A;
-`
-
-const Text = styled.p`
-	font-weight: 500;
-	font-size: 14px;
-	color: #74798c;
-	margin-left: 10px;
-`
-
-const Icon = styled.div`
-	height: 8px;
-	width: 8px;
-	border-radius: 50%;
-	background-color: ${(props) => props.color};
-	align-self: center;
-	margin-left: 30px;
-`
-
-const Legend = styled.div`
-	display: flex;
-`
-
-const Info = styled.div`
-    display: flex;
-    align-items:center;
-`
+import { Wrapper, Head, Title, Legend, Info, Icon, Text } from './BarChartStyle';
 
 interface Payload {
   value: number;
@@ -143,7 +92,7 @@ const UserBarChart = () => {
         </Legend>
       </Head>
       <ResponsiveContainer height={200}>
-        <BarChart data={userActivityData} barGap={8} barCategoryGap={1} margin={{ left: 0, right: 0 }}>
+        <BarChart data={userActivityData} barGap={8} barCategoryGap={1}>
           <CartesianGrid vertical={false} strokeDasharray="1 1" />
           <XAxis dataKey="day" tickLine={false} tick={CustomizedAxisTick} axisLine={{ stroke: '#DEDEDE' }} />
           <YAxis
