de5d8cdc1ee01007850434208340405c93122b1a
Hugo Chantelauze <hugo@etamin.studio>
2023-10-24 13:55:01 +0200

    wip fix with api

--- src/pages/UserPerformance/UserPerformance.tsx
+++ src/pages/UserPerformance/UserPerformance.tsx
@@ -1,86 +1,44 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import userPerformance from "../../service/mocked_data/userPerformance";
-import userMainData from "../../service/mocked_data/userMainData";
-import styled from "styled-components";
-
-const UserPerformanceContainer = styled.div`
-  margin: 0 auto;
-  padding: 2rem;
-  width: 80%;
-`;
-
-const PerformanceTitle = styled.h1`
-  font-size: 2.4rem;
-  margin-bottom: 2rem;
-`;
-
-const TableContainer = styled.div`
-  margin: 0 auto;
-  max-width: 100%;
-`;
-
-const Table = styled.table`
-  border-collapse: collapse;
-  border: 1px solid #FBECEB;
-  font-size: 1.6rem;
-  font-weight: 300;
-  width: 80%;
-`;
-
-const Th = styled.th`
-  background-color: #FBECEB;
-  padding: 1rem;
-  text-align: left;
-`;
-
-const Tr = styled.tr`
-  &:nth-child(even) {
-    background-color: #FBECEB;
-  }
-`;
-
-const Td = styled.td`
-  border: 1px solid #FBECEB;
-  padding: 1rem;
-  text-align: left;
-`;
+import { getUserPerformance } from "../../service/api/data";
 
 export default function UserPerformance() {
   const { userId } = useParams();
-  const userData = userPerformance.find((data) => data.userId.toString() === userId);
+  const [performanceData, setPerformanceData] = useState(null);
 
-  if (!userData) {
-    return <div>Utilisateur non trouvé.</div>;
-  }
+  const fetchData = async () => {
+    try {
+      if (!userId) return;
+      const response = await getUserPerformance(userId);
+      const performanceData = response.data;
 
-  const performanceData = userData.data;
-  const performanceKind = userData.kind;
-  const user = userMainData.find((user) => user.id.toString() === userId);
+      // Assurez-vous que performanceData contient bien les données que vous voulez afficher
+      console.log("Performance Data:", performanceData);
+
+      setPerformanceData(performanceData); // Mettez à jour l'état avec les données de performance
+    } catch (error) {
+      console.error("Une erreur s'est produite lors de la récupération des données de performance :", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [userId]);
 
   return (
-    <UserPerformanceContainer>
-      <PerformanceTitle>
-        Performance de {" "}
-        {user ? `${user.userInfos.firstName} ${user.userInfos.lastName}` : "Utilisateur inconnu"}
-      </PerformanceTitle>
-      <TableContainer>
-        <Table>
-          <thead>
-            <Tr>
-              <Th>Type d'activité</Th>
-              <Th>Valeur</Th>
-            </Tr>
-          </thead>
-          <tbody>
-            {performanceData.map((data, index) => (
-              <Tr key={index}>
-                <Td>{performanceKind[data.kind as keyof typeof performanceKind]}</Td>
-                <Td>{data.value}</Td>
-              </Tr>
+    <div>
+      {performanceData && (
+        <div>
+          <h2>Performance de l'utilisateur</h2>
+          <ul>
+            {performanceData.data.map((item, index) => (
+              <li key={index}>
+                {performanceData.kind[item.kind]}: {item.value}
+              </li>
             ))}
-          </tbody>
-        </Table>
-      </TableContainer>
-    </UserPerformanceContainer>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
